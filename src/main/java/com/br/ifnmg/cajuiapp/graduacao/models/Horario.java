9582c3ee7266d9d75d4182528531308b70cf24a3
unknown <gustavo.acinfo@gmail.com>
2019-09-30 15:31:17 -0300

    Implementacao classe horario

---
+++ src/main/java/com/br/ifnmg/cajuiapp/graduacao/models/Horario.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.ifnmg.cajuiapp.graduacao.models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Gustavo
+ */
+@Entity
+@Table(name="horario", schema="graduacao")
+public class Horario implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "oferta_id")
+    private Integer ofertaId;
+    
+    @Column(name = "professor_oferta_id")
+    private Integer professorOfertaId;
+    
+    private String datas;
+    
+    private String horas;
+    
+    @Column(name="created_at")
+    private Integer createdAt;
+    @Column(name="updated_at")
+    private Integer updatedAt;
+    @Column(name="created_by")
+    private Integer createdBy;
+    @Column(name="updated_by")
+    private Integer updatedBy;
+    
+// * @property Oferta $oferta
+// * @property ProfessorOferta $professorOferta
+// * @property array $datasHorario
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOfertaId() {
+        return ofertaId;
+    }
+
+    public void setOfertaId(Integer ofertaId) {
+        this.ofertaId = ofertaId;
+    }
+
+    public Integer getProfessorOfertaId() {
+        return professorOfertaId;
+    }
+
+    public void setProfessorOfertaId(Integer professorOfertaId) {
+        this.professorOfertaId = professorOfertaId;
+    }
+
+    public String getDatas() {
+        return datas;
+    }
+
+    public void setDatas(String datas) {
+        this.datas = datas;
+    }
+
+    public String getHoras() {
+        return horas;
+    }
+
+    public void setHoras(String horas) {
+        this.horas = horas;
+    }
+
+    public Integer getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Integer createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Integer updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Integer getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/br/ifnmg/cajuiapp/graduacao/resources/HorarioResource.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.ifnmg.cajuiapp.graduacao.resources;
+
+import com.br.ifnmg.cajuiapp.graduacao.models.Horario;
+import com.br.ifnmg.cajuiapp.graduacao.repository.HorarioRepository;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Gustavo
+ */
+@RestController
+@RequestMapping("/horario")
+public class HorarioResource {
+    
+    @Autowired
+    private HorarioRepository er;
+    
+    @GetMapping(produces="application/json")
+    public @ResponseBody Iterable listaHorarios(){
+        Iterable<Horario> listaHorarios = er.findAll();
+        return listaHorarios;
+    }
+    
+    @PostMapping()
+    public Horario cadastrarHorario(@RequestBody @Valid Horario horario){
+        return er.save(horario);
+    }
+    
+    @DeleteMapping()
+    public Horario deletaHorario(@RequestBody Horario horario){
+        er.delete(horario);
+        return horario;
+    }
+    
+}
