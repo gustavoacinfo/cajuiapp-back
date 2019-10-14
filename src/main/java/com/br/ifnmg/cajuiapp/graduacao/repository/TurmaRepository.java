806c3ada4d6bc565357068202b569009f0caf5f0
unknown <gustavo.acinfo@gmail.com>
2019-10-14 15:04:01 -0300

    Implementando metodo listar Turma do Aluno

--- src/main/java/com/br/ifnmg/cajuiapp/graduacao/repository/TurmaRepository.java
+++ src/main/java/com/br/ifnmg/cajuiapp/graduacao/repository/TurmaRepository.java
@@ -6,12 +6,18 @@
 package com.br.ifnmg.cajuiapp.graduacao.repository;
 
 import com.br.ifnmg.cajuiapp.graduacao.models.Turma;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author Gustavo
  */
 public interface TurmaRepository extends JpaRepository<Turma, String>{
+    
+     @Query(value="select * from graduacao.turma as t join graduacao.contrato as c "
+             + "on c.turma_id = t.id where c.aluno_id = ?",nativeQuery=true)
+    public List<Turma> listarTurmaDoAluno(Integer id);
     
 }
--- src/main/java/com/br/ifnmg/cajuiapp/graduacao/resources/TurmaResource.java
+++ src/main/java/com/br/ifnmg/cajuiapp/graduacao/resources/TurmaResource.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,12 @@ public class TurmaResource {
     public @ResponseBody Iterable listaTurmas(){
         Iterable<Turma> listaTurmas = er.findAll();
         return listaTurmas;
+    }
+    
+    @GetMapping(produces="application/json", value="/aluno/{id}")
+    public @ResponseBody Iterable listarTurmaDoAluno(@PathVariable("id") Integer id){
+        Iterable<Turma> listaTurmaDoAluno = er.listarTurmaDoAluno(id);
+        return listaTurmaDoAluno;
     }
     
     @PostMapping()
