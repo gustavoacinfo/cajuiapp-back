5f15b05f502f3f896c8581ab89745fa75b6d382a
unknown <gustavo.acinfo@gmail.com>
2019-10-14 21:53:13 -0300

    Implementando metodo Listar Alunos da Disciplina

--- src/main/java/com/br/ifnmg/cajuiapp/basico/repository/AlunoRepository.java
+++ src/main/java/com/br/ifnmg/cajuiapp/basico/repository/AlunoRepository.java
@@ -6,12 +6,21 @@
 package com.br.ifnmg.cajuiapp.basico.repository;
 
 import com.br.ifnmg.cajuiapp.basico.models.Aluno;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author Gustavo
  */
 public interface AlunoRepository extends JpaRepository<Aluno, String>  {
+    
+    @Query(value="select p.nome from basico.pessoa as p join basico.aluno as al on p.id = al.id join graduacao.contrato as c\n" +
+"on c.aluno_id = al.id join graduacao.matricula as m on c.id = m.contrato_id join graduacao.curriculo as cu\n" +
+"on cu.id = m.curriculo_id join graduacao.disciplina as d on cu.disciplina_id = d.id join graduacao.oferta as o\n" +
+"on m.oferta_id = o.id join graduacao.professor_oferta as po on o.id = po.oferta_id where po.professor_id = ?1 \n" +
+"and d.id = ?2 order by p.nome",nativeQuery=true)
+    public List<String> listarAlunoDaDisciplina(Integer id, Integer disciplina_id);
     
 }
--- src/main/java/com/br/ifnmg/cajuiapp/basico/resources/AlunoResource.java
+++ src/main/java/com/br/ifnmg/cajuiapp/basico/resources/AlunoResource.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,12 @@ public class AlunoResource {
     public @ResponseBody Iterable listaAlunos(){
         Iterable<Aluno> listaAlunos = er.findAll();
         return listaAlunos;
+    }
+    
+     @GetMapping(produces="application/json", value="/professor/{id}/disciplina/{disciplina_id}")
+    public @ResponseBody Iterable listaAlunoDaDisciplina(@PathVariable("id") Integer id, @PathVariable("disciplina_id") Integer disciplina_id ){
+        Iterable<String> listarAlunoDaDisciplina = er.listarAlunoDaDisciplina(id, disciplina_id);
+        return listarAlunoDaDisciplina;
     }
     
     @PostMapping()
